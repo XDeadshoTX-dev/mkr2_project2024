7ba1f3f2c5c92b0d7dade694881183e404c8e5c4
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-05-31 15:45:18 +0300

    Добавьте файлы проекта.

---
+++ mkr2_project2024/project_recipe/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from recipe import views
+
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('recipe_detail/', views.recipe_detail, name='recipe_detail'),
+    path('admin/', admin.site.urls),
+]
---
+++ mkr2_project2024/recipe/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from datetime import datetime
+from .models import Recipe
+from datetime import datetime, timezone
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the gallery index.")
+
+def main(request):
+    recipe = Recipe.objects.create(
+            title="Recept 1",
+            description="Desc for 1",
+            instructions="Instruction",
+            ingredients="Ingridients for 1",
+            created_at = timezone.now(),
+            updated_at = timezone.now(),
+            category_id=1,
+        )
+    recipe.save()
+
+    return render(
+        request,
+        'main.html',
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
+
+def recipe_detail(request):
+    return render(
+        request,
+        'recipe_detail.html',
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
