1914f70b591747e8bcfaae26dc354f06b5a2d454
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-05-31 15:53:53 +0300

    Added recipes data

--- mkr2_project2024/recipe/views.py
+++ mkr2_project2024/recipe/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from datetime import datetime
+from datetime import date, datetime
 from .models import Recipe, Category
 from datetime import datetime, timezone
 
@@ -9,29 +9,31 @@ def index(request):
     return HttpResponse("Hello, world. You're at the gallery index.")
 
 def main(request):
-    category = Category.objects.create(
-            name="For Pineapple",
-        )
-    category.save()
-    category = Category.objects.all()
-    q = len(category)
+    # category = Category.objects.create(
+    #         name="For Pineapple",
+    #     )
+    # category.save()
+    # category = Category.objects.all()
+    # q = len(category)
     # recipe = Recipe.objects.create(
-    #         title="Recept 1",
-    #         description="Desc for 1",
+    #         title="Recept 2",
+    #         description="Desc for 2",
     #         instructions="Instruction",
-    #         ingredients="Ingridients for 1",
-    #         created_at = timezone.now(),
-    #         updated_at = timezone.now(),
-    #         category_id=1,
+    #         ingredients="Ingridients for 2",
+    #         created_at = datetime.now(),
+    #         updated_at = datetime.now(),
+    #         category_id=2,
     #     )
     # recipe.save()
-
+    recipes = Recipe.objects.all()
+    # q = len(recipes)
     return render(
         request,
         'main.html',
         {
             'title': 'Home Page',
             'year': datetime.now().year,
+            'recipes': recipes,
         }
     )
 
