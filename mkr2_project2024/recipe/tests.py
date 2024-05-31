3a75d402f6a607bf6af17b2b1ec8832815d491b9
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-05-31 16:14:54 +0300

    Added tests

--- mkr2_project2024/recipe/tests.py
+++ mkr2_project2024/recipe/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe, Category
 
-# Create your tests here.
+class RecipeViewsTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="For Pineapple")
+        self.recipe = Recipe.objects.create(
+            title="Recept 2",
+            description="Desc for 2",
+            instructions="Instruction",
+            ingredients="Ingridients for 2",
+            category=self.category,
+        )
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.recipe.title, 'Recept 2')
+        #self.assertContains(response, f"Recipe Name: {self.recipe.title}")
+
+    def test_recipe_detail_view(self):
+        response = self.client.get(reverse('recipe_detail', args=[self.recipe.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.recipe.title)
